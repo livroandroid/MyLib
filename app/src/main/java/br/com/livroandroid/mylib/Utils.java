86ed1e6ea20743fc0b66dd68662849fcf9800f66
livroandroidup@gmail.com <livroandroidup@gmail.com>
2015-01-11 15:22:23 -0200

    Initial commit

---
+++ app/src/main/java/br/com/livroandroid/mylib/Utils.java
@@ -0,0 +1,17 @@
+package br.com.livroandroid.mylib;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by ricardo on 11/01/15.
+ */
+public class Utils {
+    public static String hello() {
+        return "Hello World from Lib";
+    }
+
+    public static void toast(Context context, String msg) {
+        Toast.makeText(context,msg,Toast.LENGTH_SHORT).show();
+    }
+}
